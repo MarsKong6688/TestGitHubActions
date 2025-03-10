b658f7ae8598220092910bca975fafff9ac1af60
Mars Kong <marsjiang@aslcn.com.cn>
2025-03-10 15:58:12 +0800

    Init Spring Boot app

---
+++ src/main/java/com/test/githubactions/GithubActionsApplicatoin.java
@@ -0,0 +1,13 @@
+package com.test.githubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class GithubActionsApplicatoin {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsApplicatoin.class, args);
+	}
+}
